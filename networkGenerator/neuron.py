739653b1b72a400738c9c516f605ad6f6d6a3e8f
ShravanTata <tatarama@biorobpc14>
2018-07-01 00:17:35 +0200

    Added base neuron class

---
+++ networkGenerator/neuron.py
@@ -0,0 +1,128 @@
+"""Implementation of different neuron models."""
+import biolog
+import numpy as np
+
+
+class neuron(object):
+    """Base neuron class.
+
+    """
+
+    def __init__(self, neuron_type):
+        super(neuron, self).__init__()
+        self._neuron_type = neuron_type  # : Type of neuron
+
+    @property
+    def neuron_type(self):
+        """Neuron type.  """
+        return self._neuron_type
+
+    @neuron_type.setter
+    def neuron_type(self, value):
+        """
+        Parameters
+        ----------
+        value : <str>
+            Type of neuron model
+
+        """
+        self._neuron_type = value
+
+
+class LeakyIntegrateFire(neuron):
+    """Leaky Integrate and Fire neuron.
+    """
+
+    def __init__(self, tau=0.1, D=1., bias=0.0):
+        super(LeakyIntegrateFire, self).__init__(
+            neuron_type='leaky_integrate_fire')
+        self._tau = tau
+        self._D = D
+        self._bias = bias
+        self.membrane = 0.0
+
+    @property
+    def tau(self):
+        """Neuron time constant  """
+        return self._tau
+
+    @tau.setter
+    def tau(self, value):
+        """
+        Parameters
+        ----------
+        value : <float>
+            Time constant
+        """
+        self._tau = value
+
+    @property
+    def bias(self):
+        """Neuron activation bias  """
+        return self._bias
+
+    @bias.setter
+    def bias(self, value):
+        """
+        Parameters
+        ----------
+        value : <float>
+            Neuron activation bias
+        """
+        self._bias = value
+
+    @property
+    def D(self):
+        """Neuron D  """
+        return self._D
+
+    @D.setter
+    def D(self, value):
+        """
+        Parameters
+        ----------
+        value : <float>
+            Neuron D
+        """
+        self._D = value
+
+    def ode(self, _in):
+        """
+        Parameters
+        ----------
+        _in : <np.1darray>
+            Input currents to the neuron
+
+        Returns
+        -------
+        out : <float>
+            Rate of change of neuron membrane potential
+            tau*dm\dt = -m + sum(_in)
+        """
+        return (-self.membrane + np.sum(_in)) / self.tau
+
+    def activation(self, _in):
+        """
+        Parameters
+        ----------
+        _in : <float>
+            Membrane potential of the neuron
+
+        Returns
+        -------
+        out : <float>
+            Activation value of the neuron
+        """
+        return 1 / (1 + np.exp(-self.D * _in))
+
+
+def main():
+    neuron1 = LeakyIntegrateFire()
+    biolog.debug('Neuron type : {}'.format(neuron1.neuron_type))
+    biolog.debug('Neuron ode out : {}'.format(neuron1.ode(np.arange(0, 5))))
+    biolog.debug('Neuron activation out : {}'.format(
+        neuron1.activation(neuron1.ode(np.arange(0, 5)))))
+
+
+if __name__ == '__main__':
+    main()
