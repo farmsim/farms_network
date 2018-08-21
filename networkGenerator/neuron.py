4349150dd81414e50712d048e6631df5a57dcb00
ShravanTata <tatarama@biorobpc14.epfl.ch>
2018-08-21 18:08:54 +0200

    Moved IF neuron model position in file

--- networkGenerator/neuron.py
+++ networkGenerator/neuron.py
@@ -103,6 +103,69 @@ class Neuron(object):
             'neuron_out : Method not implemented in child class')
 
 
+class IntegrateAndFire(Neuron):
+    """Integrate & Fire Neuron Model."""
+
+    def __init__(self, n_id, dae, **kwargs):
+        """Initialize.
+
+        Parameters
+        ----------
+        n_id: str
+            Unique ID for the neuron in the network.
+        """
+        super(IntegrateAndFire, self).__init__(
+            neuron_type='if')
+
+        #: Neuron ID
+        self.n_id = n_id
+        self.dae = dae
+
+        #: Initialize parameters
+        self.tau = self.dae.add_c('tau_' + self.n_id,
+                                  kwargs.get('tau', 0.1))
+        self.bias = self.dae.add_c('bias_' + self.n_id,
+                                   kwargs.get('bias', -2.75))
+        #: pylint: disable=invalid-name
+        self.D = self.dae.add_c('D_' + self.n_id,
+                                kwargs.get('D', 1.0))
+
+        #: Initialize states
+        self.m = self.dae.add_x('m_' + self.n_id,
+                                kwargs.get('x0', 0.0))
+        #: External inputs
+        self.ext_in = self.dae.add_u('ext_in_' + self.n_id)
+
+        #: ODE RHS
+        self.mdot = self.dae.add_ode('mdot_' + self.n_id, 0.0)
+        self.ode_rhs()
+
+    def ode_add_input(self, neuron, **kwargs):
+        """ Add relevant external inputs to the ode."""
+        weight = self.dae.add_p(
+            'w_' + neuron.n_id + '_to_' + self.n_id,
+            kwargs.get('weight'))
+        self.mdot.sym += (
+            neuron.neuron_output()*weight.sym)/self.tau.sym
+        return
+
+    def ode_rhs(self):
+        """ Generate the ODE. Internal Setup Function."""
+        self.mdot.sym = (
+            -self.m.sym + self.ext_in.sym)/self.tau.sym
+
+    def neuron_output(self):
+        """Neuron activation function.
+        Parameters
+        ----------
+        m_potential: float
+            Neuron membrane potential
+        """
+        exp = np.exp  # pylint: disable = no-member
+        return 1. / (1. + exp(-self.D.sym * (
+            self.m.sym + self.bias.sym)))
+
+
 class LIF_Danner_Nap(Neuron):
     """Leaky Integrate and Fire Neuron Based on Danner et.al.
 
@@ -373,69 +436,6 @@ class LIF_Danner(Neuron):
         _cond = cas.logic_and(self.v_thr <= self.v, self.v < self.v_max)
         _f = (self.v - self.v_thr) / (self.v_max - self.v_thr)
         return cas.if_else(_cond, _f, 1.) * (self.v > self.v_thr)
-
-
-class IntegrateAndFire(Neuron):
-    """Integrate & Fire Neuron Model."""
-
-    def __init__(self, n_id, dae, **kwargs):
-        """Initialize.
-
-        Parameters
-        ----------
-        n_id: str
-            Unique ID for the neuron in the network.
-        """
-        super(IntegrateAndFire, self).__init__(
-            neuron_type='if')
-
-        #: Neuron ID
-        self.n_id = n_id
-        self.dae = dae
-
-        #: Initialize parameters
-        self.tau = self.dae.add_c('tau_' + self.n_id,
-                                  kwargs.get('tau', 0.1))
-        self.bias = self.dae.add_c('bias_' + self.n_id,
-                                   kwargs.get('bias', -2.75))
-        #: pylint: disable=invalid-name
-        self.D = self.dae.add_c('D_' + self.n_id,
-                                kwargs.get('D', 1.0))
-
-        #: Initialize states
-        self.m = self.dae.add_x('m_' + self.n_id,
-                                kwargs.get('x0', 0.0))
-        #: External inputs
-        self.ext_in = self.dae.add_u('ext_in_' + self.n_id)
-
-        #: ODE RHS
-        self.mdot = self.dae.add_ode('mdot_' + self.n_id, 0.0)
-        self.ode_rhs()
-
-    def ode_add_input(self, neuron, **kwargs):
-        """ Add relevant external inputs to the ode."""
-        weight = self.dae.add_p(
-            'w_' + neuron.n_id + '_to_' + self.n_id,
-            kwargs.get('weight'))
-        self.mdot.sym += (
-            neuron.neuron_output()*weight.sym)/self.tau.sym
-        return
-
-    def ode_rhs(self):
-        """ Generate the ODE. Internal Setup Function."""
-        self.mdot.sym = (
-            -self.m.sym + self.ext_in.sym)/self.tau.sym
-
-    def neuron_output(self):
-        """Neuron activation function.
-        Parameters
-        ----------
-        m_potential: float
-            Neuron membrane potential
-        """
-        exp = np.exp  # pylint: disable = no-member
-        return 1. / (1. + exp(-self.D.sym * (
-            self.m.sym + self.bias.sym)))
 
 
 class LIF_Daun_Interneuron(Neuron):
