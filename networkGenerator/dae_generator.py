8fcae00503475e35fb73f4db8fe6e5f94522e606
ShravanTata <shravan.tataramalingasetty@epfl.ch>
2018-08-23 08:44:45 +0200

    Incomplete work on dae gen

--- networkGenerator/dae_generator.py
+++ networkGenerator/dae_generator.py
@@ -1,62 +1,72 @@
-""" Describe/Generate DAE Model."""
+""" Describe/Generate DAE Model. """
 
 from collections import OrderedDict
 import casadi as cas
 import biolog
 
 
-class Parameters(OrderedDict):
+class Parameters(list):
     """Book keeping for casadi objects.
     """
 
     def __init__(self):
         """ Initialization. """
         super(Parameters, self).__init__()
+        self._name_to_idx = {}  #: Dict to store name, index
         return
 
-    def vals(self):
-        """ Get list parameter values. """
-        from IPython import embed
-        embed()
+    def get(self, key):
+        """ Get the attribute value by name."""
+        if key not in self._name_to_idx:
+            raise AttributeError()
+        else:
+            return self[self._name_to_idx[key]]
 
-        return self[0]._val
+    def set(self, key, value):
+        """ Set the attribute value by name."""
+        if key not in self._name_to_idx:
+            raise AttributeError()
+        else:
+            self[self._name_to_idx[key]] = value
 
-    def syms(self):
-        """ Get list parameter symbolics. """
-        return [val.sym for val in self.values()]
+    def add(self, name, sym, value):
+        """Add new element to the parameter list.
+        Parameters
+        ----------
+        name : <str>
+             Name of the parameter
 
-    def set_all(self, val):
-        """ Set all the parameters in the list. """
-        for key in self.keys():
-            self[key].val = val
+        sym : <cas.SX.sym>
+             Symbolic casadi expression
 
+        value : <float>
+             Value to be set for the parameter
+
+        Returns
+        -------
+        out : <bool>
+            Returns true if successful
+        """
+
+        _idx = len(self)
+        self._name_to_idx[name] = _idx
+        self.insert(_idx, Param(sym=sym, value=value))
+        return
 
 class Param(object):
     """Wrapper for parameters in the system.
     Can be a casadi symbolic or numeric value.
     """
 
-    def __init__(self, sym, value=None, p_list=None, idx=None):
+    def __init__(self, sym, value=None):
         """ Initialization. """
         super(Param, self).__init__()
-        self.idx = idx
-        self._val = p_list
-        if self._val is not None:
-            self._val.append(value)
+
+        #: Update the value to the list
+        self.val = value
+        #: Store the symbol
         self.sym = sym
         return
-
-    @property
-    def val(self):
-        """Value  """
-        return self._val[self.idx]
-
-    @val.setter
-    def val(self, data):
-        """
-        Param
-        """
-        self._val[self.idx] = data
 
 
 class DaeGenerator(object):
@@ -69,21 +79,16 @@ class DaeGenerator(object):
 
         #: Variables
         #: pylint: disable=invalid-name
-        self.x = []  #: States
-        self.z = []  #: Algebraic variables
-        self.p = []  #: []       self.u = []  #: Inputs
-        self.c = []  #: Named Constants
-        self.y = []  #: Outputs
-
-        self.x_l = []  #: States
-        self.z_l = []  #: Algebraic variables
-        self.p_l = []  #: []       self.u = []  #: Inputs
-        self.c_l = []  #: Named Constants
-        self.y_l = []  #: Outputs
+        self.x = Parameters()  #: States
+        self.z = Parameters()  #: Algebraic variables
+        self.p = Parameters()  #: Parameters
+        self.u = Parameters()  #: Inputs
+        self.c = Parameters()  #: Named Constants
+        self.y = Parameters()  #: Outputs
 
         #: Equations
-        self.alg = []  #: Algebraic equations
-        self.ode = []  #: ODE RHS
+        self.alg = Parameters()  #: Algebraic equations
+        self.ode = Parameters()  #: ODE RHS
 
     def add_x(self, name, value=0.0):
         """Add a new state.
@@ -93,6 +98,7 @@ class DaeGenerator(object):
         value : <float>
             Initial value for the state variable
         """
+
         self.x.append(Param(cas.SX.sym(name), value, self.x_l,
                             len(self.x_l)))
         return self.x[-1]
@@ -117,9 +123,10 @@ class DaeGenerator(object):
         value : <float>
             Default parameter value to be used during run time
         """
-        self.p.append(Param(cas.SX.sym(
-            name), value, self.p_l, len(self.p_l))
-        return self.p[-1]
+        # self.p.append(Param(cas.SX.sym(
+        #     name), value, self.p_l, len(self.p_l))
+        # return self.p[-1]
+        pass
 
     def add_u(self, name, value=0.0):
         """Add a new Input.
@@ -129,9 +136,10 @@ class DaeGenerator(object):
         value : <float>
             Default parameter value to be used during run time
         """
-        self.u.append(Param(cas.SX.sym(
-            name), value, self.u_l, len(self.u_l))
-        return self.u[-1]
+        # self.u.append(Param(cas.SX.sym(
+        #     name), value, self.u_l, len(self.u_l))
+        # return self.u[-1]
+        pass
 
     def add_c(self, name, value=0.0):
         """Add a new constant.
@@ -141,9 +149,10 @@ class DaeGenerator(object):
         value : <float>
             Default parameter value to be used during run time
         """
-        self.c.append(Param(cas.SX.sym(
-            name), value, self.c_l, len(self.c_l))
-        return self.c[-1]
+        # self.c.append(Param(cas.SX.sym(
+        #     name), value, self.c_l, len(self.c_l))
+        # return self.c[-1]
+        pass
 
     def add_ode(self, name, ode):
         """Add a new ode rhs.
