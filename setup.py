4cba4d1eb69413ba3e91dcd377b27fda37cad0bf
ShravanTata <tatarama@biorobpc14.epfl.ch>
2018-08-24 17:14:46 +0200

    Added setup file to install the network generation module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='network_generator',
+      version='0.1',
+      description='Module generate, develop and visualize neural networks',
+      url='https://gitlab.com/BioRobAnimals/NetworkGenerator.git',
+      author='Shravan Tata Ramalingasetty',
+      author_email='shravantr@gmail.com',
+      license='MIT',
+      packages=['networkGenerator'],
+      zip_safe=False)
