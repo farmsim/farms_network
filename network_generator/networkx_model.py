059c700f79de26b15e50feceeb4c5d982d28bc5f
ShravanTata <shravan.tataramalingasetty@epfl.ch>
2018-08-25 01:54:03 +0200

    Removed pyqt5 matplotlib for scrolling

--- network_generator/networkx_model.py
+++ network_generator/networkx_model.py
@@ -2,46 +2,12 @@
 
 import os
 
-import matplotlib
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
-from matplotlib.backends.backend_qt5agg import \
-    FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import \
-    NavigationToolbar2QT as NavigationToolbar
 from networkx.drawing.nx_pydot import write_dot
-from PyQt5 import QtWidgets
 
 import biolog
-
-# Make sure that we are using QT5
-matplotlib.use('Qt5Agg')
-
-
-class ScrollableWindow(QtWidgets.QMainWindow):
-    def __init__(self, fig):
-        self.qapp = QtWidgets.QApplication([])
-
-        QtWidgets.QMainWindow.__init__(self)
-        self.widget = QtWidgets.QWidget()
-        self.setCentralWidget(self.widget)
-        self.widget.setLayout(QtWidgets.QVBoxLayout())
-        self.widget.layout().setContentsMargins(0, 0, 0, 0)
-        self.widget.layout().setSpacing(0)
-
-        self.fig = fig
-        self.canvas = FigureCanvas(self.fig)
-        self.canvas.draw()
-        self.scroll = QtWidgets.QScrollArea(self.widget)
-        self.scroll.setWidget(self.canvas)
-
-        self.nav = NavigationToolbar(self.canvas, self.widget)
-        self.widget.layout().addWidget(self.nav)
-        self.widget.layout().addWidget(self.scroll)
-
-        self.show()
-        exit(self.qapp.exec_())
 
 
 class NetworkXModel(object):
