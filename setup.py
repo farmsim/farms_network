f4c7185956bd749298c5ca79d8bdc9b4e7e1be4d
ShravanTata <shravan.tataramalingasetty@epfl.ch>
2018-08-25 01:21:10 +0200

    Edited and update setup file for new repo name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+import setuptools
 
-setup(name='network_generator',
-      version='0.1',
-      description='Module generate, develop and visualize neural networks',
-      url='https://gitlab.com/BioRobAnimals/NetworkGenerator.git',
-      author='Shravan Tata Ramalingasetty',
-      author_email='shravantr@gmail.com',
-      license='MIT',
-      packages=['network_generator'],
-      zip_safe=False)
+setuptools.setup(name='network_generator',
+                 version='0.1',
+                 description='Module generate, develop and visualize neural networks',
+                 url='https://gitlab.com/BioRobAnimals/NetworkGenerator.git',
+                 author='Shravan Tata Ramalingasetty',
+                 author_email='shravantr@gmail.com',
+                 license='MIT',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False)
