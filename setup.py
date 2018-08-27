55e466d28774b865bc0aa5d6066562895d6882cb
ShravanTata <tatarama@biorobpc14.epfl.ch>
2018-08-27 15:31:36 +0200

    Updated git url in setup file

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setuptools
 setuptools.setup(name='network_generator',
                  version='0.1',
                  description='Module generate, develop and visualize neural networks',
-                 url='https://gitlab.com/BioRobAnimals/NetworkGenerator.git',
+                 url='https://gitlab.com/BioRobAnimals/network_generator.git',
                  author='Shravan Tata Ramalingasetty',
                  author_email='shravantr@gmail.com',
                  license='MIT',
                  packages=setuptools.find_packages(
-                    exclude=['tests*']),
+                     exclude=['tests*']),
                  zip_safe=False)
