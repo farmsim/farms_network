bea4d72b94b48442668ab07cde32f2f529b2d127
SimonDanner <simon.danner@gmail.com>
2018-09-04 18:22:38 -0400

    elife bifurcation diagram

--- tests/danner_models/e_life/cpg_danner_network_e_life.py
+++ tests/danner_models/e_life/cpg_danner_network_e_life.py
@@ -5,8 +5,11 @@ import time
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+import numpy.matlib as npml
 from matplotlib import rc
 import os
+from scipy.stats import circstd, circmean
+
 
 import biolog
 from danner_net_gen_e_life import (CPG, LPSN, Commissural, ConnectFore2Hind,
@@ -21,6 +24,61 @@ plt.rc('axes', titlesize=14.0)     # fontsize of the axes title
 plt.rc('axes', labelsize=14.0)    # fontsize of the x and y labels
 plt.rc('xtick', labelsize=14.0)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=14.0)    # fontsize of the tick labels
+
+def calc_phase(time_vec,out):
+    out = np.piecewise(out,[out<=-50,out>=0,np.logical_and(out>-50.0, out<0.0)],[0,1,lambda x:(x+50.0)/50.0])
+    os_=((np.diff((out>0.1).astype(np.int),axis=0)==1).T)
+    onsets=npml.repmat(time_vec[:-1],4,1)[os_]
+    leg=(npml.repmat(np.arange(4.0),len(time_vec)-1,1).T)[os_]
+    times=np.stack((onsets,leg),1)
+    times=times[times[:,0].argsort()]
+
+    pdur=np.diff(times[(times[:,1]==0),0])
+    phases=np.zeros((3))
+    std_phases=np.zeros((3))
+    for i,(x,y) in enumerate([(0,1),(0,2),(0,3)]):
+        times_=times[(times[:,1]==x) | (times[:,1]==y)]
+        indices = np.where((times_[:-2,1]==x) & (times_[1:-1,1]==y) & (times_[2:,1]==x))
+        pdur_=times_[[ind+2 for ind in indices],0]-times_[indices,0]
+        phases_=((times_[[ind+1 for ind in indices],0]-times_[indices,0])/pdur_).T
+        phases[i]=circmean(phases_[-6:-1],1,0)
+        std_phases[i]=circstd(phases_[-6:-1],1,0)
+
+    fq = 1/np.mean(pdur[-6:-1])
+    return (fq,phases,np.max(std_phases))
+
+def run_bifurcation(network,alphas,stepdur):
+    dt = 1  #: Time step
+    time_vec = np.arange(0, stepdur, dt)  #: Time
+    
+
+    frequencies = np.zeros((len(alphas)))*np.nan
+    phases = np.zeros((len(alphas),3))*np.nan
+
+    inRGF_fr=network.dae.x.get_idx('V_FR_RG_F')
+    inRGF_fl=network.dae.x.get_idx('V_FL_RG_F')
+    inRGF_hr=network.dae.x.get_idx('V_HR_RG_F')
+    inRGF_hl=network.dae.x.get_idx('V_HL_RG_F')
+    #phase_diffs = [(inRGF_hl,inRGF_hr),(inRGF_hl,inRGF_fl),(inRGF_hl,inRGF_fr)]
+    indices=(inRGF_hl,inRGF_hr,inRGF_fl,inRGF_fr)
+    network.dae.u.set_all_val(alphas[0])
+    for idx in range(1000):
+        network.step()
+
+    for i,alpha in enumerate(alphas):
+        network.dae.u.set_all_val(alpha)
+        res = np.empty([len(time_vec), len(network.dae.x)])
+        for idx, _ in enumerate(time_vec):
+            res[idx] = network.step()['xf'].full()[:, 0]
+        
+        fq_, phases_, stdp = calc_phase(time_vec*1e-3,res[:,indices])
+        #import IPython; IPython.embed()
+        frequencies[i]=fq_
+        phases[i,:] = phases_
+        biolog.info('Finished interation {}'.format(i))
+
+    return (frequencies,phases)
+        
 
 
 def main():
@@ -85,8 +143,8 @@ def main():
             "enable_jacobian": True,
             "print_time": False,
             "print_stats": False,
-            "reltol": 1e-6,
-            "abstol": 1e-6}
+            "reltol": 1e-4,
+            "abstol": 1e-5}
 
     # #: Setup the integrator
     net_.setup_integrator(integration_method='cvodes',
@@ -113,6 +171,28 @@ def main():
 
     biolog.info('Execution Time : {}'.format(
         end_time - start_time))
+
+    alphas=np.linspace(0.02,1.05,100)
+    fqs,phases = run_bifurcation(net_,alphas,2000)
+
+    alphas2=np.linspace(1.05,0.02,100)
+    fqs2,phases2 = run_bifurcation(net_,alphas2,2000)
+
+    _, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, sharex='all')
+    ax1.plot(alphas,fqs, 'b', linewidth=1)
+    ax1.plot(alphas2,fqs2, 'r', linewidth=1)
+    ax2.plot(alphas,phases[:,0], 'b.')
+    ax2.plot(alphas,1.0-phases[:,0], 'b.')
+    ax2.plot(alphas2,phases2[:,0], 'r.')
+    ax2.plot(alphas2,1.0-phases2[:,0], 'r.')
+    ax2.set_ylim([0.0,1.0])
+    ax3.plot(alphas,phases[:,1], 'b.')
+    ax3.plot(alphas2,phases2[:,1], 'r.')
+    ax3.set_ylim([0.0,1.0])
+    ax4.plot(alphas,phases[:,2], 'b.')
+    ax4.plot(alphas2,phases2[:,2], 'r.')
+    ax4.set_ylim([0.0,1.0])
+    plt.show()
 
     # #: Results
     net_.save_network_to_dot()
