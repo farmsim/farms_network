a65945fe411d5b207057b13bc4332ef06d17be85
ShravanTata <shravan.tataramalingasetty@epfl.ch>
2018-12-03 15:24:06 +0100

    Fixed motorneuron naming error and motorneuron class eqn error

--- network_models/daun_cpg/daun_net_gen.py
+++ network_models/daun_cpg/daun_net_gen.py
@@ -188,7 +188,7 @@ class Motorneurons(object):
     def add_neurons(self, anchor_x, anchor_y, color):
         """ Add neurons. """
         self.motorneurons.add_node(self.name+'_MN1',
-                                   model='lif_daun_motorneruon',
+                                   model='lif_daun_motorneuron',
                                    x=-3.0+anchor_x,
                                    y=1.5+anchor_y,
                                    color=color,
