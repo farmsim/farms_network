38463a3d6da0d621e53b6bfd546f1e1bcbd5979e
Shravan Tata Ramalingasetty <tatarama@Shravans-MacBook-Pro.local>
2019-10-12 22:36:05 +0200

    Converted test_neuron to unittest Neuron base class

--- tests/unit/test_neuron.py
+++ tests/unit/test_neuron.py
@@ -1,83 +1,41 @@
 """Unit test of for generic neuron model."""
 
 import unittest
-import ddt
-from farms_network.neuron_factory import NeuronFactory
-from farms_container import Container
-import numpy as np
 import farms_pylog as pylog
-
+from farms_network.neuron import Neuron
 # pylog.set_level('debug')
 
-def neuron_types():
-    """ Return all the muscle types implemented. """
-    return (list(NeuronFactory.neurons.keys()))
-
-@ddt.ddt
-class TestNeuronModel(unittest.TestCase):
-    """Test different neuron models in the framework.
+class TestNeuronBaseClass(unittest.TestCase):
+    """Test neuron base class
     """
 
-    def setUp(self):
-        """ Set up the neruon model. """
-        container = Container()
-        container.add_namespace('neural')
-        #: ODE States
-        container.neural.add_table('states')
-        container.neural.add_table('dstates')
-        #: Neurons parameters
-        container.neural.add_table(
-            'constants', TABLE_TYPE='CONSTANT')
-        container.neural.add_table('parameters')
-        #: Input to neural system
-        container.neural.add_table('inputs')
-        #: Secondary outputs 
-        container.neural.add_table('outputs')        
-
-    def tearDown(self):
-        """ Tear up neuron model. """
-        pylog.debug("Tear down...")
-        Container.del_instance()
-
-    @ddt.data(*neuron_types())
-    def test_neuron_instance(self, value):
+    def test_neuron_instance(self):
         """ Test neurons instances. """
-        pylog.debug("Initializing neurons {}".format(value))
-        Neuron = NeuronFactory.gen_neuron(value)
-        neuron = Neuron("test_neuron", 0)
-        container = Container.get_instance()
-        container.initialize()
+        neuron = Neuron("test_neuron")
         self.assertIsInstance(neuron, Neuron)
 
-    @ddt.data(*neuron_types())
-    def test_neuron_output_method_exists(self, value):
+    def test_neuron_output_abstract_method(self):
         """ Test if neuron output method exists . """
-        pylog.debug("Initializing neurons {}".format(value))
-        Neuron = NeuronFactory.gen_neuron(value)
-        neuron = Neuron("test_neuron", 0)
-        container = Container.get_instance()
-        container.initialize()
-        self.assertTrue(hasattr(neuron, 'output'))
+        neuron = Neuron("test_neuron")
+        with self.assertRaises(NotImplementedError):
+            neuron.output()
 
-    @ddt.data(*neuron_types())
-    def test_neuron_ode_rhs_method_exists(self, value):
+    def test_neuron_ode_rhs_abstract_method(self):
         """ Test if neuron has ode_rhs method implemented """
-        pylog.debug("Initializing neurons {}".format(value))
-        Neuron = NeuronFactory.gen_neuron(value)
-        neuron = Neuron("test_neuron", 0)
-        container = Container.get_instance()
-        container.initialize()
-        self.assertTrue(hasattr(neuron, 'ode_rhs'))
+        neuron = Neuron("test_neuron")
+        with self.assertRaises(NotImplementedError):
+            neuron.ode_rhs(None, None)
 
-    @ddt.data(*neuron_types())
-    def test_neuron_add_ode_input_exists(self, value):
+    def test_neuron_add_ode_input_exists(self):
         """ Test neurons add_ode_input. """
-        pylog.debug("Initializing neurons {}".format(value))
-        Neuron = NeuronFactory.gen_neuron(value)
-        neuron = Neuron("test_neuron", 0)
-        container = Container.get_instance()
-        container.initialize()
-        self.assertTrue(hasattr(neuron, 'add_ode_input'))
+        neuron = Neuron("test_neuron")
+        with self.assertRaises(NotImplementedError):
+            neuron.add_ode_input(None)
 
-if __name__ == '__main__':        
+    def test_neuron_model_type(self):
+        """ Test neuron model type. """
+        neuron = Neuron("test_neuron")
+        self.assertIn(neuron.model_type, "test_neuron")
+
+if __name__ == '__main__':
     unittest.main()
