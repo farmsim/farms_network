1e1fa41f6cbf2c67a71a9078fe6a1cdc912ec18c
Savyaraj <savya.deshmukh@gmail.com>
2019-10-18 01:13:40 +0200

    Update neuron_factory.py

--- farms_network/neuron_factory.py
+++ farms_network/neuron_factory.py
@@ -8,6 +8,7 @@ from farms_network.sensory_neuron import SensoryNeuron
 from farms_network.leaky_integrator import LeakyIntegrator
 from farms_network.oscillator import Oscillator
 from farms_network.fitzhugh_nagumo import FitzhughNagumo
+from farms_network.matsuoka_neuron import MatsuokaNeuron
 
 
 class NeuronFactory(object):
@@ -22,6 +23,7 @@ class NeuronFactory(object):
             'lif_daun_interneuron': LIFDaunInterneuron,
             'hh_daun_motorneuron': HHDaunMotorneuron,
             'fitzhugh_nagumo': FitzhughNagumo,
+            'matsuoka_neuron': MatsuokaNeuron,
         }
     
     def __init__(self):
