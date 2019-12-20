c8fbc2b2fc94e58e8598de9512edaf206da16fa3
ShravanTata <tatarama@biorobpc14.epfl.ch>
2019-12-20 20:51:11 +0100

    [WIP] Added morphed_oscillator template

---
+++ farms_network/morphed_oscillator.pyx
@@ -0,0 +1,161 @@
+# cython: cdivision=True
+# cython: language_level=3
+# cython: infer_types=True
+# cython: profile=False
+# cython: boundscheck=False
+# cython: wraparound=False
+# cython: nonecheck=False
+# cython: initializedcheck=False
+# cython: overflowcheck=False
+# cython: optimize.unpack_method_calls=True
+# cython: np_pythran=False
+
+"""Morphed Oscillator model"""
+from farms_container import Container
+from libc.stdio cimport printf
+import farms_pylog as pylog
+from libc.math cimport exp
+from libc.math cimport M_PI
+from libc.math cimport sin as csin
+import numpy as np
+cimport numpy as cnp
+
+
+cdef class MorphedOscillator(Neuron):
+
+    def __init__(self, n_id, num_inputs, **kwargs):
+        """Initialize.
+
+        Parameters
+        ----------
+        n_id: str
+            Unique ID for the neuron in the network.
+        """
+        super(MorphedOscillator, self).__init__('leaky')
+
+        #: Neuron ID
+        self.n_id = n_id
+        #: Get container
+        container = Container.get_instance()
+
+        #: Initialize parameters
+        (_, self.f) = container.neural.constants.add_parameter(
+            'f_' + self.n_id, kwargs.get('f', 0.5))
+
+        (_, self.gamma) = container.neural.constants.add_parameter(
+            'g_' + self.n_id, kwargs.get('gamma', 100))
+
+        (_, self.K) = container.neural.constants.add_parameter(
+            'K_' + self.n_id, kwargs.get('K', 1))
+
+        #: Initialize states
+        self.theta = container.neural.states.add_parameter(
+            'theta_' + self.n_id, kwargs.get('theta0', 0.0))[0]
+        self.x = container.neural.states.add_parameter(
+            'x_' + self.n_id, kwargs.get('x0', 0.0))[0]
+        
+        #: External inputs
+        self.ext_in = container.neural.inputs.add_parameter(
+            'ext_in_' + self.n_id)[0]
+
+        #: ODE RHS
+        self.theta_dot = container.neural.dstates.add_parameter(
+            'theta_dot_' + self.n_id, 0.0)[0]
+        self.x_dot = container.neural.dstates.add_parameter(
+            'x_dot_' + self.n_id, 0.0)[0]
+
+        #: Output
+        self.nout = container.neural.outputs.add_parameter(
+            'nout_' + self.n_id, 0.0)[0]
+
+        #: Neuron inputs
+        self.neuron_inputs = cnp.ndarray((num_inputs,),
+                                         dtype=[('neuron_idx', 'i'),
+                                                ('weight_idx', 'i'),
+                                                ('theta_idx', 'i')])
+
+        self.num_inputs = num_inputs
+
+    def add_ode_input(self,int idx, neuron, **kwargs):
+        """ Add relevant external inputs to the ode."""
+        #: Create a struct to store the inputs and weights to the neuron
+        cdef MorphedOscillatorNeuronInput n
+        container = Container.get_instance()
+        #: Get the neuron parameter
+        neuron_idx = container.neural.outputs.get_parameter_index(
+            'nout_'+neuron.n_id)
+
+        #: Add the weight parameter
+        weight = container.neural.weights.add_parameter(
+            'w_' + neuron.n_id + '_to_' + self.n_id,
+            kwargs.get('weight', 0.0))[0]
+        phi = container.neural.parameters.add_parameter(
+            'phi_' + neuron.n_id + '_to_' + self.n_id,
+            kwargs.get('phi', 0.0))[0]
+
+        weight_idx = container.neural.weights.get_parameter_index(
+            'w_' + neuron.n_id + '_to_' + self.n_id)
+        phi_idx = container.neural.parameters.get_parameter_index(
+            'phi_' + neuron.n_id + '_to_' + self.n_id)
+
+        n.neuron_idx = neuron_idx
+        n.weight_idx = weight_idx
+        n.phi_idx = phi_idx
+
+        #: Append the struct to the list
+        self.neuron_inputs[idx] = n
+
+    def output(self):
+        """Neuron activation function.
+        Parameters
+        ----------
+        m_potential: float
+            Neuron membrane potential
+        """
+        return self.c_output()
+
+    def ode_rhs(self, y, w, p):
+        """ Python interface to the ode_rhs computation."""
+        self.c_ode_rhs(y, w, p)
+
+    #################### C-FUNCTIONS ####################
+    cdef void c_ode_rhs(self, double[:] _y, double[:] _w, double[:] _p) nogil:
+        """ Compute the ODE. Internal Setup Function."""
+
+        #: Current state
+        cdef double _theta = self.theta.c_get_value()
+        cdef double _x = self.x.c_get_value()
+
+        #: Neuron inputs
+        cdef double _sum = 0.0
+        cdef unsigned int j
+        cdef double _neuron_out
+        cdef double _weight
+        cdef double _theta
+
+        for j in range(self.num_inputs):
+            _neuron_out = _y[self.neuron_inputs[j].neuron_idx]
+            _weight = _w[self.neuron_inputs[j].weight_idx]
+            _phi = _p[self.neuron_inputs[j].phi_idx]
+            _sum += self.c_neuron_inputs_eval(_neuron_out,
+                                              _weight, _theta, _phi)
+
+        #: thetadot : theta_dot
+        self.theta_dot.c_set_value(2*M_PI*self.f + _sum)
+
+        #: xdot
+        self.x_dot.c_set_value(self.a*(self.R - _amp))
+
+    cdef void c_output(self) nogil:
+        """ Neuron output. """
+        self.nout.c_set_value(self.x.c_get_value())
+
+    cdef double c_neuron_inputs_eval(
+            self, double _neuron_out, double _weight, double _theta,
+            double _phi) nogil:
+        """ Evaluate neuron inputs."""
+        return _weight*csin(_neuron_out - _theta - _phi)
+
+    cdef double c_function_approximator(double p0, double p1, double p3):
+        """ Approximates a function as a 3rd order polynomial. """ 
+        return 0.0
