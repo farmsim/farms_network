15194004bf745ec2544e36eb0e01b6c560d360e8
ShravanTata <tatarama@biorobpc14.epfl.ch>
2020-02-20 20:12:26 +0100

    [MOUSE] Added removal of nodes from the default controller

--- animals/mouse/scripts/quadruped_locomotion.py
+++ animals/mouse/scripts/quadruped_locomotion.py
@@ -16,14 +16,129 @@ def main():
     controller_gen = AgnosticController(
         ("../../../../farms_blender/animats/"
          "mouse_v1/design/sdf/mouse_locomotion.sdf"),
-        connect_closest_neighbors=False,
-        connect_base_nodes=False,
-    )    
+    )
     net_dir = "../config/quadruped_locomotion.graphml"
-
+    network = controller_gen.network
     #: EDIT THE GENERIC CONTROLLER
-    
-    nx.write_graphml(controller_gen.network, net_dir)
+    #: Remove MTP nodes
+    network.remove_nodes_from(
+        [
+            'LMtp_flexion',
+            'RMtp_flexion',
+            'LMtp_extension',
+            'RMtp_extension'
+        ]
+    )
+    #: Remove Palm nodes
+    network.remove_nodes_from(
+        [
+            'LPalm_flexion',
+            'RPalm_flexion',
+            'LPalm_extension',
+            'RPalm_extension'
+        ]
+    )
+    #: Remove Head nodes
+    network.remove_nodes_from(
+        [
+            'Head_flexion',
+            'Head_extension',
+        ]
+    )
+    #: Remove Cervical nodes
+    network.remove_nodes_from(
+        [
+            'Cervical_flexion',
+            'Cervical_extension',
+        ]
+    )
+    #: Connect hind-fore limbs
+    AgnosticController.add_mutual_connection(
+        network,
+        'LHip_flexion',
+        'RHip_flexion',
+        weight=10.0,
+        phi=np.pi
+    )
+    AgnosticController.add_mutual_connection(
+        network,
+        'LShoulder_flexion',
+        'RShoulder_flexion',
+        weight=10.0,
+        phi=np.pi
+    )
+    AgnosticController.add_mutual_connection(
+        network,
+        'LHip_flexion',
+        'RShoulder_flexion',
+        weight=10.0,
+        phi=0.0
+    )
+    AgnosticController.add_mutual_connection(
+        network,
+        'RHip_flexion',
+        'LShoulder_flexion',
+        weight=10.0,
+        phi=0.0
+    )
+    AgnosticController.add_mutual_connection(
+        network,
+        'RHip_flexion',
+        'RShoulder_flexion',
+        weight=10.0,
+        phi=np.pi/2
+    )
+    AgnosticController.add_mutual_connection(
+        network,
+        'LHip_flexion',
+        'LShoulder_flexion',
+        weight=10.0,
+        phi=np.pi/2
+    )
+    AgnosticController.add_mutual_connection(
+        network,
+        'LHip_extension',
+        'RHip_extension',
+        weight=10.0,
+        phi=np.pi
+    )
+    AgnosticController.add_mutual_connection(
+        network,
+        'LShoulder_extension',
+        'RShoulder_extension',
+        weight=10.0,
+        phi=np.pi
+    )
+    AgnosticController.add_mutual_connection(
+        network,
+        'LHip_extension',
+        'RShoulder_extension',
+        weight=10.0,
+        phi=0.0
+    )
+    AgnosticController.add_mutual_connection(
+        network,
+        'RHip_extension',
+        'LShoulder_extension',
+        weight=10.0,
+        phi=0.0
+    )
+    AgnosticController.add_mutual_connection(
+        network,
+        'RHip_extension',
+        'RShoulder_extension',
+        weight=10.0,
+        phi=np.pi/2
+    )
+    AgnosticController.add_mutual_connection(
+        network,
+        'LHip_extension',
+        'LShoulder_extension',
+        weight=10.0,
+        phi=np.pi/2
+    )
+
+    nx.write_graphml(network, net_dir)
 
     # #: Initialize network
     dt = 0.001  #: Time step
@@ -33,6 +148,7 @@ def main():
     net = NeuralSystem(
         "../config/quadruped_locomotion.graphml",
         container)
+
     #: initialize network parameters
     container.initialize()
     net.setup_integrator()
@@ -50,17 +166,23 @@ def main():
     neuron_out = np.asarray(container.neural.outputs.log)
     names = container.neural.outputs.names
     parameters = container.neural.parameters
+
     #: Show graph
     print(net.graph.number_of_edges())
+    print(net.graph.number_of_nodes())
     net.visualize_network(edge_labels=False)
     nosc = net.network.graph.number_of_nodes()
-    plt.figure()    
+
+    plt.figure()
     for j in range(nosc):
-        plt.plot((state[:, 2*j+1]*np.sin(neuron_out[:, j])))
+        plt.plot(j + (state[:, 2*j+1]*np.sin(neuron_out[:, j])))
     plt.legend(names)
     plt.grid(True)
-    plt.show()
 
+    plt.figure()
+    plt.plot(state[:, 0::2])
+    plt.grid(True)
+    plt.show()
 
 if __name__ == '__main__':
     main()
