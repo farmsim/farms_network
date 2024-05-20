ff21a2a5e8e6ee4fc3ebe19bde35c67d5368de8f
Shravan Tata <shravantr@gmail.com>
2024-05-20 14:22:50 -0400

    [MAIN] Updated setup file to compile all core and data cython modules

--- setup.py
+++ setup.py
@@ -33,109 +33,15 @@ Options.closure_freelist_size = 8
 # directive_defaults = Cython.Compiler.Options.get_directive_defaults()
 
 extensions = [
-    Extension("farms_network.network_generator",
-              ["farms_network/network_generator.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.oscillator",
-              ["farms_network/oscillator.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.hopf_oscillator",
-              ["farms_network/hopf_oscillator.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.morphed_oscillator",
-              ["farms_network/morphed_oscillator.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.leaky_integrator",
-              ["farms_network/leaky_integrator.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.neuron",
-              ["farms_network/neuron.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.lif_danner_nap",
-              ["farms_network/lif_danner_nap.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.lif_danner",
-              ["farms_network/lif_danner.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.lif_daun_interneuron",
-              ["farms_network/lif_daun_interneuron.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.hh_daun_motorneuron",
-              ["farms_network/hh_daun_motorneuron.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.sensory_neuron",
-              ["farms_network/sensory_neuron.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.fitzhugh_nagumo",
-              ["farms_network/fitzhugh_nagumo.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.matsuoka_neuron",
-              ["farms_network/matsuoka_neuron.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.morris_lecar",
-              ["farms_network/morris_lecar.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.relu",
-              ["farms_network/relu.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.integrators",
-              ["farms_network/integrators.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3']
-              ),
-    Extension("farms_network.utils.ornstein_uhlenbeck",
-              ["farms_network/utils/ornstein_uhlenbeck.pyx"],
-              include_dirs=[numpy.get_include(), get_include()],
-              # libraries=["c", "stdc++"],
-              extra_compile_args=['-ffast-math', '-O3'],
-              extra_link_args=['-O3'],
-              )
+    Extension(
+        f"farms_network.{subpackage}.*",
+        [f"farms_network/{subpackage}/*.pyx"],
+        include_dirs=[numpy.get_include(), get_include()],
+        # libraries=["c", "stdc++"],
+        extra_compile_args=['-ffast-math', '-O3'],
+        extra_link_args=['-O3'],
+    )
+    for subpackage in ('core', 'data')
 ]
 
 setup(
@@ -191,6 +97,5 @@ setup(
     ),
     package_data={
         'farms_network': ['*.pxd'],
-        'farms_container': ['*.pxd'],
     },
 )
