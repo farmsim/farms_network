6b52d280052fff274b50369a07ba63cf8a2e04ae
Shravan Tata <shravantr@gmail.com>
2024-09-30 10:31:21 -0400

    [SCRATCH] Updated test_network to changes in base function signatures

--- scratch/test_network.py
+++ scratch/test_network.py
@@ -5,16 +5,24 @@ from farms_network.core.network import PyNetwork
 from farms_network.core.options import NetworkOptions
 from scipy.integrate import ode
 
-nstates = 100
-network = PyNetwork.from_options(nnodes=nstates)
+nnodes = 100
+
+states = np.zeros((nnodes,))
+dstates = np.zeros((nnodes,))
+outputs = np.zeros((nnodes,))
+weights = np.zeros((nnodes,))
+
+network = PyNetwork(nnodes=nnodes)
 
 integrator = ode(network.ode).set_integrator(
     u'dopri5',
     method=u'adams',
     max_step=0.0,
+    nsteps=4
 )
-integrator.set_initial_value(np.zeros((nstates,)), 0.0)
+integrator.set_initial_value(np.zeros((nnodes,)), 0.0)
 
+integrator.integrate(integrator.t+1.0)
 
 # # Integrate
 # for iteration in range(0, 100):
