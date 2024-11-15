7356ea4a1b0b2c0d126782ea8abed6901d150ef9
Shravan Tata <shravantr@gmail.com>
2024-11-15 00:54:32 -0500

    [UTILS] Added console script to load and run from config file

---
+++ farms_network/utils/run.py
@@ -0,0 +1,43 @@
+""" Run script """
+
+from argparse import ArgumentParser
+
+from farms_core.io.yaml import read_yaml
+from farms_network.core.network import PyNetwork
+from farms_network.core.options import NetworkOptions
+from tqdm import tqdm
+
+
+def run_network(network_options):
+
+    network = PyNetwork.from_options(network_options)
+    network.setup_integrator(network_options.integration)
+
+    # data.to_file("/tmp/sim.hdf5")
+
+    # Integrate
+    N_ITERATIONS = network_options.integration.n_iterations
+    TIMESTEP = network_options.integration.timestep
+
+    inputs_view = network.data.external_inputs.array
+    for iteration in tqdm(range(0, N_ITERATIONS), colour="green", ascii=" >="):
+        inputs_view[:] = (iteration / N_ITERATIONS) * 1.0
+        network.step()
+        network.data.times.array[iteration] = iteration*TIMESTEP
+
+
+def main():
+    """ Main """
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--config_path", "-c", dest="config_path", type=str, required=True
+    )
+    clargs = parser.parse_args()
+    # run network
+    options = NetworkOptions.from_options(read_yaml(clargs.config_path))
+    run_network(options)
+
+
+if __name__ == '__main__':
+    main()
