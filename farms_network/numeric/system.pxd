20a7fb13e62fd903d34aab875b1a21cbe36b84c2
Shravan Tata <shravantr@gmail.com>
2024-11-20 23:01:55 -0500

    [NUMERIC] Added stochastic differential equation system base class

--- farms_network/numeric/system.pxd
+++ farms_network/numeric/system.pxd
@@ -1,3 +1,9 @@
 cdef class ODESystem:
 
     cdef void evaluate(self, double time, double[:] states, double[:] derivatives) noexcept
+
+
+cdef class SDESystem:
+
+    cdef void evaluate_a(self, double time, double[:] states, double[:] derivatives) noexcept
+    cdef void evaluate_b(self, double time, double[:] states, double[:] derivatives) noexcept
--- farms_network/numeric/system.pyx
+++ farms_network/numeric/system.pyx
@@ -11,3 +11,19 @@ cdef class ODESystem:
     cdef void evaluate(self, double time, double[:] states, double[:] derivatives) noexcept:
         """ Evaluate that needs to filled out by an ODE system """
         ...
+
+
+cdef class SDESystem:
+    """ SDE system of the form: dXt = a(Xt,t) dt + b(Xt,t) dW,"""
+
+    def __init__(self):
+        """ Initialize """
+        ...
+
+    cdef void evaluate_a(self, double time, double[:] states, double[:] derivatives) noexcept:
+        """ a(Xt,t) """
+        ...
+
+    cdef void evaluate_b(self, double time, double[:] states, double[:] derivatives) noexcept:
+        """ b(Xt,t) """
+        ...
