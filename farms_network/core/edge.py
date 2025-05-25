2d70a4a61845ae7da8b5b461aa47ec962c254724
Shravan Tata <shravantr@gmail.com>
2025-05-24 21:20:12 -0400

    [CORE] Refactored edge class

--- farms_network/core/edge.py
+++ farms_network/core/edge.py
@@ -2,19 +2,32 @@
 
 from farms_network.core.edge_cy import EdgeCy
 from farms_network.core.options import EdgeOptions
+from farms_network.models import EdgeTypes
+from typing import Dict
 
 
 class Edge(EdgeCy):
     """ Interface to edge class """
 
-    def __init__(self, source: str, target: str, edge_type: str, **kwargs):
-        super().__init__(source, target, edge_type, **kwargs)
+    def __init__(self, source: str, target: str, edge_type: EdgeTypes, **kwargs):
+        self.source: str = source
+        self.target: str = target
+        self._edge_cy = EdgeCy(source, target, edge_type)
+
+    @property
+    def edge_type(self):
+        return self._edge_cy.type
+
+    @edge_type.setter
+    def edge_type(self, edge_type: EdgeTypes):
+        self._edge_cy.type = edge_type
 
     @classmethod
     def from_options(cls, edge_options: EdgeOptions):
         """ From edge options """
         source: str = edge_options.source
         target: str = edge_options.target
-        edge_type: str = edge_options.type
-        parameter_options = {} if edge_options.parameters is None else edge_options.parameters
+        edge_type: EdgeTypes = edge_options.type
+        # Need to generate parameters based on the model specified
+        parameter_options: Dict = {} # if edge_options.parameters is None else edge_options.parameters
         return cls(source, target, edge_type, **parameter_options)
--- farms_network/core/edge_cy.pxd
+++ farms_network/core/edge_cy.pxd
@@ -11,21 +11,14 @@ cdef enum:
 
 
 cdef struct edge_t:
-
-    #
-    char* source                # Source node
-    char* target                # Target node
     unsigned int type           # Type of connection
-    char* model                 # Type of the model (e.g., "base")
-
     # Edge parameters
-    unsigned int nparameters
-    void* parameters
-
+    unsigned int nparams
+    void* params
 
 
 cdef class EdgeCy:
-    """ Python interface to Edge C-Structure"""
+    """ Python interface to Edge C-Structure """
 
     cdef:
         edge_t* _edge
